ed9b56aa44914f58d1651157b83c9867559c0003
Jeannie Nguyen <Jeannienguyen888@gmail.com>
2026-08-18 14:16:00 -0700

    Add class for inside boxes and boarders
    Class Style allows easy identification for
    all inside boxes

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,7 +2,8 @@
 let refToBoxes = document.querySelector(".containsSqrs")
 for (let x = 0; x < 16; x++) {
     for (let y = 0; y < 16; y++) {
-        let xy = document.createElement("div")
+        let xy = (document.createElement("div"))
+        xy.classList.add("insideBox")
         xy.textContent = x + " " + y
         refToBoxes.append(xy)
     }
