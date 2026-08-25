fd7e3702ca6f81619e3be6d1858d7c1eb5434c54
Jeannie Nguyen <Jeannienguyen888@gmail.com>
2026-08-25 14:07:01 -0700

    Mouse can change button background color
    Add hover over event listener to all buttons
    to set the background color to specific color

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -3,8 +3,12 @@ let refToBoxes = document.querySelector(".containsSqrs")
 for (let x = 0; x < 16; x++) {
     for (let y = 0; y < 16; y++) {
         let xy = (document.createElement("div"))
+        xy.addEventListener("mouseover", backgroundChange)
         xy.classList.add("insideBox")
-        //xy.textContent = x + " " + y
         refToBoxes.append(xy)
     }
 }   
+
+function backgroundChange(e){
+    e.target.style.backgroundColor = "purple"
+}
